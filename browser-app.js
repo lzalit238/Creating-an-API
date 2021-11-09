478a014cac21fcb6e564f9cf46e02374d39bb519
lzalit238 <lzalit238@west-mec.org>
2021-11-09 15:21:50 -0700

    API data and index.html

---
+++ browser-app.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const http = require('http');
+const app = express()
+const { programs } = require('./data');
+
+app.use(express.static('./public'))
+
+app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '/index.html'))
+})
+
+app.get('/api/programs', (req, res) => {
+    const newPrograms = programs.map((program) => {
+        const { id, name, desc } = programs
+        return { id, name, desc}
+    })
+    res.json(newPrograms)
+})
+
+app.listen(5000, () => {
+    console.log('Server is listening on port 5000....')
+})
+// const server = http.createServer((req, res) => {
+//     //console.log(req.method)
+//     const url = req.url
+//     //home page
+//     if (url === '/') {
+//         res.writeHead(200, { 'content-type': 'text/html' })
+//         res.write('<h1>Home Page </h1>')
+//         res.end()
+//     }
+//     //about page
+//     else if (url === '/about') {
+//         res.writeHead(200, { 'content-type': 'text/html' })
+//         res.write('<h1>About Page</h1>')
+//         res.end()
+//     }
+//     //404
+//     else {
+//         res.writeHead(404, { 'content-type': 'text/html' })
+//         res.write('<h1>Page not found</h1>')
+//         res.end()
+//     }
+// })
---
+++ data.js
@@ -0,0 +1,373 @@
+const programs = [
+    {
+        id: 1,
+        name: '3-D Animation',
+        desc: '',
+    },
+    {
+        id: 2,
+        name: 'Accounting I',
+        desc: '',
+    },
+    {
+        id: 3,
+        name: 'Accounting II',
+        desc: '',
+    },
+    {
+        id: 4,
+        name: 'Advertising',
+        desc: '',
+    },
+    {
+        id: 5,
+        name: 'Agribusiness',
+        desc: '',
+    },
+    {
+        id: 6,
+        name: 'American Enterprise Project',
+        desc: '',
+    },
+    {
+        id: 7,
+        name: 'Banking & Financial Systems',
+        desc: '',
+    },
+    {
+        id: 8,
+        name: 'Broadcast Journalism',
+        desc: '',
+    },
+    {
+        id: 9,
+        name: 'Business Calculations',
+        desc: '',
+    },
+    {
+        id: 10,
+        name: 'Business Communications',
+        desc: '',
+    },
+    {
+        id: 11,
+        name: 'Business Ethics – MODIFIED',
+        desc: '',
+    },
+    {
+        id: 12,
+        name: 'Business Financial Plan',
+        desc: '',
+    },
+    {
+        id: 13,
+        name: 'Business Law',
+        desc: '',
+    },
+    {
+        id: 14,
+        name: 'Business Management – NAME CHANGE',
+        desc: '',
+    },
+    {
+        id: 15,
+        name: 'Business Plan',
+        desc: '',
+    },
+    {
+        id: 16,
+        name: 'Client Service',
+        desc: '',
+    },
+    {
+        id: 17,
+        name: 'Coding & Programming – MODIFIED',
+        desc: '',
+    },
+    {
+        id: 18,
+        name: 'Community Service Project',
+        desc: '',
+    },
+    {
+        id: 19,
+        name: 'Computer Applications',
+        desc: '',
+    },
+    {
+        id: 20,
+        name: 'Computer Game & Simulation Programming',
+        desc: '',
+    },
+    {
+        id: 21,
+        name: 'Computer Problem Solving',
+        desc: '',
+    },
+    {
+        id: 22,
+        name: 'Cyber Security',
+        desc: '',
+    },
+    {
+        id: 23,
+        name: 'Data Analysis - NEW',
+        desc: '',
+    },
+    {
+        id: 24,
+        name: 'Database Design & Application',
+        desc: '',
+    },
+    {
+        id: 25,
+        name: 'Digital Video Production',
+        desc: '',
+    },
+    {
+        id: 26,
+        name: 'E-business - MODIFIED',
+        desc: '',
+    },
+    {
+        id: 27,
+        name: 'Economics',
+        desc: '',
+    },
+    {
+        id: 28,
+        name: 'Electronic Career Portfolio',
+        desc: '',
+    },
+    {
+        id: 29,
+        name: 'Entrepreneurship',
+        desc: '',
+    },
+    {
+        id: 30,
+        name: 'Future Business Leader',
+        desc: '',
+    },
+    {
+        id: 31,
+        name: 'Graphic Design',
+        desc: '',
+    },
+    {
+        id: 32,
+        name: 'Health Care Administration',
+        desc: '',
+    },
+    {
+        id: 33,
+        name: 'Help Desk',
+        desc: '',
+    },
+    {
+        id: 34,
+        name: 'Hospitality & Event Management',
+        desc: '',
+    },
+    {
+        id: 35,
+        name: 'Human Resource Management - NEW',
+        desc: '',
+    },
+    {
+        id: 36,
+        name: 'Impromptu Speaking',
+        desc: '',
+    },
+    {
+        id: 37,
+        name: 'Insurance & Risk Management',
+        desc: '',
+    },
+    {
+        id: 38,
+        name: 'International Business',
+        desc: '',
+    },
+    {
+        id: 39,
+        name: 'Introduction to Business Communication',
+        desc: '',
+    },
+    {
+        id: 40,
+        name: 'Introduction to Business Concepts - NAME CHANGE',
+        desc: '',
+    },
+    {
+        id: 41,
+        name: 'Introduction to Business Presentation',
+        desc: '',
+    },
+    {
+        id: 42,
+        name: 'Introduction to Business Procedures',
+        desc: '',
+    },
+    {
+        id: 43,
+        name: 'Introduction to Event Planning',
+        desc: '',
+    },
+    {
+        id: 44,
+        name: 'Introduction to FBLA',
+        desc: '',
+    },
+    {
+        id: 45,
+        name: 'Introduction to Financial Math',
+        desc: '',
+    },
+    {
+        id: 46,
+        name: 'Introduction to Information Technology',
+        desc: '',
+    },
+    {
+        id: 47,
+        name: 'Introduction to Parlimentary Procedure',
+        desc: '',
+    },
+    {
+        id: 48,
+        name: 'Introduction to Public Speaking',
+        desc: '',
+    },
+    {
+        id: 49,
+        name: 'Introduction to Social Media Strategy',
+        desc: '',
+    },
+    {
+        id: 50,
+        name: 'Job Interview',
+        desc: '',
+    },
+    {
+        id: 51,
+        name: 'Journalism',
+        desc: '',
+    },
+    {
+        id: 52,
+        name: 'Local Chapter Annual Business Report',
+        desc: '',
+    },
+    {
+        id: 53,
+        name: 'Management Information Systems',
+        desc: '',
+    },
+    {
+        id: 54,
+        name: 'Marketing',
+        desc: '',
+    },
+    {
+        id: 55,
+        name: 'Mobile Application Development',
+        desc: '',
+    },
+    {
+        id: 56,
+        name: 'Network Design',
+        desc: '',
+    },
+    {
+        id: 57,
+        name: 'Networking Infrastructures',
+        desc: '',
+    },
+    {
+        id: 54,
+        name: 'Organizational Leadership',
+        desc: '',
+    },
+    {
+        id: 55,
+        name: 'Parliamentary Procedure',
+        desc: '',
+    },
+    {
+        id: 56,
+        name: 'Partnership with Business Project',
+        desc: '',
+    },
+    {
+        id: 57,
+        name: 'Personal Finance',
+        desc: '',
+    },
+    {
+        id: 58,
+        name: 'Political Science',
+        desc: '',
+    },
+    {
+        id: 59,
+        name: 'Public Service Announcement',
+        desc: '',
+    },
+    {
+        id: 60,
+        name: 'Public Speaking',
+        desc: '',
+    },
+    {
+        id: 61,
+        name: 'Publication Design',
+        desc: '',
+    },{
+        id: 62,
+        name: 'Sales Presentation',
+        desc: '',
+    },{
+        id: 63,
+        name: 'Securities & Investments',
+        desc: '',
+    },
+    {
+        id: 64,
+        name: 'Social Media Strategies',
+        desc: '',
+    },
+    {
+        id: 65,
+        name: 'Sports & Entertainment Management',
+        desc: '',
+    },
+    {
+        id: 66,
+        name: 'Spreadsheet Applications',
+        desc: '',
+    },
+    {
+        id: 67,
+        name: 'Supply Chain Management',
+        desc: '',
+    },
+    {
+        id: 68,
+        name: 'UX Design - NEW',
+        desc: '',
+    },
+    {
+        id: 69,
+        name: 'Website Design',
+        desc: '',
+    },
+    {
+        id: 70,
+        name: 'Word Processing',
+        desc: '',
+    },
+]
+
+
+module.exports = { programs }
